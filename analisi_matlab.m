ccffdab5616a6b1ec193d6b88a077ed88b7d704a
lutonelli <61193571+lutonelli@users.noreply.github.com>
2021-05-17 16:37:56 +0200

    Aggiunta analisi per Windows, ma non per Linux

--- analisi_matlab.m
+++ analisi_matlab.m
@@ -1,9 +1,9 @@
 function [] = analisi_matlab(matrix_dir, matlab_file, mem_file, result_file)
 
-%%%%%%%%%%%%%%% DA TESTARE %%%%%%%%%%%%%%%
+% SU WINDOWS OK, DA TESTARE SU LINUX
 
-% da cmd
-% matlab -nodisplay -nosplash -nodesktop -r "matlab_test('Matrici-test/nome-matrice.mat', 'nome-matrice-result.txt');exit;"
+% per Windows: analisi_matlab('Testdeltest', '', '', '')
+% per Linux: specificare tutti i parametri
 
 % Come parametri si aspetta:
 % path cartella matrici 
@@ -13,25 +13,36 @@ function [] = analisi_matlab(matrix_dir, matlab_file, mem_file, result_file)
 % devono essere dati in questo ordine
 
 matrices = dir(strcat(matrix_dir,'/*.mat'));
-script2 = strcat("truncate -size -1 '", matlab_file, "'"); % elimino ultima newline inutile
-script3 = strcat("paste -d ';' '", matlab_file, "' '", mem_file, "' > '", result_file, "'"); % merge dei due file
-script4 = strcat("echo '' >> '", matlab_file, "'"); % aggiungo newline nel file prodotto dallo script Matlab
+script2 = strcat("truncate --size=1 ", matlab_file); % elimino ultima newline inutile
+script3 = strcat("paste -d ';' ", matlab_file, " ", mem_file, " > ", result_file); % merge dei due file
+script4 = strcat("echo '' >> ", matlab_file); % aggiungo newline nel file prodotto dallo script Matlab
 
 for i = 1:length(matrices)
     matrix_name = strcat(matrix_dir, "/", matrices(i).name);
-    script1 = strcat("/usr/bin/time -f '%M' matlab -W matlab-test(", matrix_name, ", ", matlab_file, ") &>> '", mem_file, "'");
     % Misurazione memoria
     if ispc % Windows
-        system(strcat("C:\cygwin64\bin\bash.exe --login -c ", script1), '-echo');
-        system(strcat("C:\cygwin64\bin\bash.exe --login -c ", script2), '-echo');
-        system(strcat("C:\cygwin64\bin\bash.exe --login -c ", script3), '-echo');
-        system(strcat("C:\cygwin64\bin\bash.exe --login -c ", script4), '-echo');
+        matlab_test(matrix_name, strcat(extractBefore(matrix_name,'.mat'), "_results.txt"));
     elseif isunix % Linux (o Mac)
+        script1 = strcat("/usr/bin/time -f '%M' matlab -W matlab-test(", matrix_name, ", ", matlab_file, ") &>> ", mem_file);
         system(script1,'-echo');
         system(script2, '-echo'); 
         system(script3,'-echo'); 
         system(script4,'-echo'); 
     end       
+end
+
+% Merge dei risultati in un unico file - in Linux già fatto
+if ispc % Windows
+    files=dir(strcat(matrix_dir, '/*.txt'));
+    fileout='merged.txt';
+    fout=fopen(fileout,'w');
+    for cntfiles=1:length(files)
+        fin=fopen(files(cntfiles).name);
+        while ~feof(fin)
+            fprintf(fout,strcat(extractBefore(files(cntfiles).name, '.txt'), ' %s %d\n'),fgetl(fin),cntfiles);
+        end
+        fclose(fin);
+    end
 end
 
 end
