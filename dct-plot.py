c01e2ecb05e969e2c16bdfc1abde9e1ed3dca58c
lucreziatonelli <lucreziatonelli17@gmail.com>
2021-06-09 16:37:58 +0200

    Plot dct2

--- dct-plot.py
+++ dct-plot.py
@@ -8,12 +8,8 @@ sns.set_theme(style="darkgrid")
 
 data = pd.read_pickle(os.path.join(".", "out-dct", "data-result.pkl"))
 
-'''
-g = sns.lineplot(data=result)
+g = sns.lmplot(data=data, x='Dimension', y='Time', hue='Type')
 g.set(yscale='log')
-values = ['2^4', '2^5', '2^6', '2^7', '2^8', '2^9', '2^10', '2^11', '2^12', '2^13', '2^14']
-plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], values)
 plt.show()
 fig = g.get_figure()
 fig.savefig("./images/plotdct2.png")
-'''
